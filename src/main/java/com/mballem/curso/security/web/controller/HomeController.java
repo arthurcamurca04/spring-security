12dfa2da831c617bcc8b232fff37fdd85e56969f
Arthur Camurça <arthur.04.pb@gmail.com>
2019-09-09 17:28:14 -0300

    login implementado

--- src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,14 +1,34 @@
 package com.mballem.curso.security.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping("/")
+	@GetMapping({"/",  "/home"})
 	public String home() {
 		return "home";
-	}	
+	}
+	
+	//abrir página login
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	//login inválido
+	@GetMapping("/login-error")
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais Inválidas");
+		model.addAttribute("texto", "Login ou Senha incorretos. Tente novamente!");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados!");
+		return "login";
+	}
+	
+	
 }
